dadafe9073adc4e86ced2922092c13fdc3fd94d0
szajek <krzysztof.szajek.mail@gmail.com>
2019-03-21 12:08:03 +0100

    Add simpson rule integration for fractional calculus

--- fractulus/equation.py
+++ fractulus/equation.py
@@ -7,7 +7,9 @@ from fdm.geometry import Point
 __all__ = ['Settings', 'create_left_caputo_stencil', 'create_right_caputo_stencil', 'create_riesz_caputo_stencil',
            'create_left_rectangle_rule_stencil', 'create_right_rectangle_rule_stencil',
            'create_riesz_rectangle_rule_stencil', 'create_left_trapezoidal_rule_stencil',
-           'create_right_trapezoidal_rule_stencil', 'create_riesz_trapezoidal_rule_stencil']
+           'create_right_trapezoidal_rule_stencil', 'create_riesz_trapezoidal_rule_stencil',
+           'create_left_simpson_rule_stencil', 'create_right_simpson_rule_stencil',
+           'create_riesz_simpson_rule_stencil']
 
 
 Settings = collections.namedtuple('CaputoSettings', ('alpha', 'lf', 'resolution'))
@@ -166,4 +168,92 @@ def create_riesz_trapezoidal_rule_stencil(settings):
         settings,
         create_left_trapezoidal_rule_stencil(settings),
         create_right_trapezoidal_rule_stencil(settings)
+    )
+
+
+def create_left_simpson_rule_stencil(settings):
+    alpha, lf, m = settings
+    dt = lf / float(m)
+    i = 0
+
+    m_is_even = math.fmod(m, 2.) == 0.
+
+    def _calc_k(number):
+        return i - m + number
+
+    def _weight_k_eq_i_sub_m():
+        a = (m ** (3 - alpha) - (m - 2) ** (3 - alpha)) / (math.gamma(4 - alpha))
+        b = - (3 * m ** (2 - alpha) + (m - 2) ** (2 - alpha)) / (2 * math.gamma(3 - alpha))
+        c = (m ** (1 - alpha)) / (math.gamma(2 - alpha))
+        return a + b + c
+
+    def _weight_odd_k_add_i_sub_m(i, k):
+        a = -2 * ((i - k + 1) ** (3 - alpha) - (i - k - 1) ** (3 - alpha)) / (math.gamma(4 - alpha))
+        b = 2 * ((i - k + 1) ** (2 - alpha) + (i - k - 1) ** (2 - alpha)) / (math.gamma(3 - alpha))
+        return a + b
+
+    def _weight_even_k_add_i_sub_m(i, k):
+        a = ((i - k + 2) ** (3 - alpha) - (i - k - 2) ** (3 - alpha)) / (math.gamma(4 - alpha))
+        b = ((i - k + 2) ** (2 - alpha) + 6 * (i - k) ** (2 - alpha) + (i - k - 2) ** (2 - alpha)) / (
+        2 * math.gamma(3 - alpha))
+        return a - b
+
+    def _weight_even_m_and_k_eq_i():
+        a = (2 ** (3 - alpha)) / (math.gamma(4 - alpha))
+        b = -(2 ** (2 - alpha)) / (2 * math.gamma(3 - alpha))
+        return a + b
+
+    def _weight_odd_m_and_k_eq_i_sub_1():
+        a = (3**(3 - alpha) - 1)/(math.gamma(4. - alpha))
+        b = -(3**(2 - alpha) + 3)/(2*math.gamma(3. - alpha))
+        c = -1./(math.gamma(2. - alpha))
+        return a + b + c
+
+    def w_i_k(number):
+        k = _calc_k(number)
+        if k == i - m:
+            return _weight_k_eq_i_sub_m()
+        elif m_is_even and k == i:
+            return _weight_even_m_and_k_eq_i()
+        elif not m_is_even and k == i - 1:
+            return _weight_odd_m_and_k_eq_i_sub_1()
+        elif math.fmod(i - m + k, 2) == 0. and k < i - 1:
+            return _weight_even_k_add_i_sub_m(i, k)
+        elif math.fmod(i - m + k, 1) == 0.:
+            return _weight_odd_k_add_i_sub_m(i, k)
+        else:
+            return 0.
+
+    def u_k(k):
+        if k == -1:
+            return (2.*(1. - alpha)**2 + 3. - 3.*alpha)/(2*math.gamma(4. - alpha))
+        elif k == 0:
+            return (4. - 2.*alpha)/(math.gamma(4. - alpha))
+        elif k == 1:
+            return (-1. + alpha)/(2.*math.gamma(4. - alpha))
+        else:
+            return 0.
+
+    def weights_odd(number):
+        k = _calc_k(number)
+        w = w_i_k(number) if k <= i - 1 else 0.
+        u = u_k(k) if k in [-1, 0, 1] else 0.
+        return w + u
+
+    multiplier = dt ** (1. - alpha)
+    if m_is_even:
+        return create_parametrized_stencil(-lf, 0., m, multiplier, w_i_k)
+    else:
+        return create_parametrized_stencil(-lf, dt, m + 1, multiplier, weights_odd)
+
+
+def create_right_simpson_rule_stencil(settings):
+    return create_left_simpson_rule_stencil(settings).symmetry(Point(0.)).multiply(-1.)
+
+
+def create_riesz_simpson_rule_stencil(settings):
+    return create_riesz_stencil(
+        settings,
+        create_left_simpson_rule_stencil(settings),
+        create_right_simpson_rule_stencil(settings)
     )
--- fractulus/test/unit/test_equation.py
+++ fractulus/test/unit/test_equation.py
@@ -1,17 +1,16 @@
-import math
 import unittest
 
-from fdm import Scheme, Stencil
+from fdm import Stencil
 from fdm.geometry import Point
 from fractulus.equation import (Settings, create_right_caputo_stencil, create_left_caputo_stencil, \
                                 create_riesz_caputo_stencil, create_left_rectangle_rule_stencil,
                                 create_right_rectangle_rule_stencil, create_left_trapezoidal_rule_stencil,
-                                create_right_trapezoidal_rule_stencil)
+                                create_right_trapezoidal_rule_stencil, create_left_simpson_rule_stencil,
+                                create_right_simpson_rule_stencil)
 
 
 class CaputoStencilTest(unittest.TestCase):
     def test_LeftSide_Always_ReturnStencilWithCorrectWeights(self):
-
         lf = 0.6
         resolution = 4
         alpha = 0.5
@@ -22,15 +21,14 @@ class CaputoStencilTest(unittest.TestCase):
         _operator = create_left_caputo_stencil(alpha, lf=lf, p=resolution)
         results = _operator._weights
 
-        expected = self._calc_expected_weights(
-            settings,
-            settings.lf,
-            0.,
-            lambda p, n, index: (p - 1.) ** index - (p - n + alpha - 1.) * p ** (n - alpha),
-            lambda p, n, index: 1.,
-            lambda p, j, index: (p - j + 1.) ** index - 2. * (p - j) ** index + (p - j - 1.) ** index,
-            lambda n: 1.,
-        )
+        expected = {
+            Point(-0.44999999999999996, 0.0, 0.0): 0.12706258982171437,
+            Point(-0.6, 0.0, 0.0): 0.057148272422657305,
+            Point(0.0, 0.0, 0.0): 0.29134624815788773,
+            Point(-0.15000000000000002, 0.0, 0.0): 0.24135913466702896,
+            Point(-0.3, 0.0, 0.0): 0.1571224994043748
+        }
+
         self.assertEqual(expected, results)
 
     def test_RightSide_Always_ReturnStencilWithCorrectWeights(self):
@@ -43,15 +41,12 @@ class CaputoStencilTest(unittest.TestCase):
         _operator = create_right_caputo_stencil(alpha, lf=lf, p=resolution)
         results = _operator._weights
 
-        expected = self._calc_expected_weights(
-            settings,
-            0.,
-            settings.lf,
-            lambda p, n, index: 1.,
-            lambda p, n, index: (p - 1.) ** index - (p - n + alpha - 1.) * p ** (n - alpha),
-            lambda p, j, index: (j + 1.) ** index - 2. * j ** index + (j - 1.) ** index,
-            lambda n: (-1.)**n
-        )
+        expected = {Point(1.0, 0.0, 0.0): -0.6231866060136243,
+                    Point(3.0, 0.0, 0.0): -0.3280741962036558,
+                    Point(-0.0, 0.0, 0.0): -0.752252778063675,
+                    Point(2.0, 0.0, 0.0): -0.4056885490050856,
+                    Point(4, 0.0, 0.0): -0.14755620490498422
+                    }
 
         self.assertEqual(expected, results)
 
@@ -67,34 +62,9 @@ class CaputoStencilTest(unittest.TestCase):
         self.assertAlmostEqual(1., result[Point(0)].real, places=5)
         self.assertAlmostEqual(1., sum([v.real for v in result.values()]), places=5)
 
-    @staticmethod
-    def _calc_expected_weights(settings, left_limit, right_limit, u_0_weight_provider, u_p_weight_provider,
-                               u_j_weight_provider, multiplier_provider):
-
-        alpha = settings.alpha
-        lf = settings.lf
-        resolution = p = settings.resolution
-        h = lf/resolution
-        n = math.floor(alpha) + 1.
-
-        multiplier = multiplier_provider(n) * (h ** (n - alpha)) / math.gamma(n - alpha + 2.)
-        index = (n - alpha + 1.)
-        u_0_weight = u_0_weight_provider(p, n, index)
-        u_p_weight = u_p_weight_provider(p, n, index)
-
-        weights = {
-            -left_limit: u_0_weight,
-            right_limit: u_p_weight,
-        }
-        for j in range(1, p):
-            relative_node_address = -left_limit + j * h
-            weights[relative_node_address] = u_j_weight_provider(p, j, index)
-        return {Point(relative_address): multiplier * weight for relative_address, weight in weights.items()}
-
 
 class RectangleRuleStencilTest(unittest.TestCase):
     def test_Create_Left_ReturnCorrectStencil(self):
-
         result = create_left_rectangle_rule_stencil(Settings(alpha=0.5, lf=0.8, resolution=4))
 
         expected = Stencil({
@@ -107,7 +77,6 @@ class RectangleRuleStencilTest(unittest.TestCase):
         self.assertEqual(expected, result)
 
     def test_Create_Right_ReturnCorrectStencil(self):
-
         result = create_right_rectangle_rule_stencil(Settings(alpha=0.5, lf=0.8, resolution=4))
 
         expected = Stencil({
@@ -122,7 +91,6 @@ class RectangleRuleStencilTest(unittest.TestCase):
 
 class TrapezoidalRuleStencilTest(unittest.TestCase):
     def test_Create_Left_ReturnCorrectStencil(self):
-
         result = create_left_trapezoidal_rule_stencil(Settings(alpha=0.5, lf=0.8, resolution=4))
 
         expected = Stencil({
@@ -135,8 +103,7 @@ class TrapezoidalRuleStencilTest(unittest.TestCase):
 
         self.assertEqual(expected, result)
 
-    def _test_Create_Right_ReturnCorrectStencil(self):
-
+    def test_Create_Right_ReturnCorrectStencil(self):
         result = create_right_trapezoidal_rule_stencil(Settings(alpha=0.5, lf=0.8, resolution=4))
 
         expected = Stencil({
@@ -145,6 +112,64 @@ class TrapezoidalRuleStencilTest(unittest.TestCase):
             Point(0.4): -0.1814294346537252,
             Point(0.6): -0.14671924087499555,
             Point(0.8): -0.06598914093388651,
+        })
+
+        self.assertEqual(expected, result)
+
+
+class SimpsonRuleStencilTest(unittest.TestCase):
+    def test_Create_LeftEvenResolution_ReturnCorrectStencil(self):
+        result = create_left_simpson_rule_stencil(Settings(alpha=0.5, lf=0.8, resolution=4))
+
+        expected = Stencil({
+            Point(-0.8): 0.04139442395762801,
+            Point(-0.6): 0.19590867482751353,
+            Point(-0.4): 0.10587690665922159,
+            Point(-0.2): 0.38061314477925734,
+            Point(-0.0): 0.28545985858444356
+        })
+
+        self.assertEqual(expected, result)
+
+    def test_Create_RightEvenResolution_ReturnCorrectStencil(self):
+        result = create_right_simpson_rule_stencil(Settings(alpha=0.5, lf=0.8, resolution=4))
+
+        expected = Stencil({
+            Point(0.): -0.28545985858444356,
+            Point(0.2): -0.38061314477925734,
+            Point(0.4): -0.10587690665922159,
+            Point(0.6): -0.19590867482751353,
+            Point(0.8): -0.04139442395762801,
+        })
+
+        self.assertEqual(expected, result)
+
+    def test_Create_LeftOddResolution_ReturnCorrectStencil(self):
+        result = create_left_simpson_rule_stencil(Settings(alpha=0.5, lf=1.0, resolution=5))
+
+        expected = Stencil({
+            Point(-1.0): 0.03727938104424598,
+            Point(-0.8): 0.1690131707314828,
+            Point(-0.6): 0.09488746599316666,
+            Point(-0.4): 0.24273847930859488,
+            Point(-0.2): 0.214401233455065,
+            Point(-0.0): 0.40370120352322564,
+            Point(0.2): -0.033641766960268805,
+        })
+
+        self.assertEqual(expected, result)
+
+    def test_Create_RightOddResolution_ReturnCorrectStencil(self):
+        result = create_right_simpson_rule_stencil(Settings(alpha=0.5, lf=1.0, resolution=5))
+
+        expected = Stencil({
+            Point(-0.2): 0.033641766960268805,
+            Point(0.): -0.40370120352322564,
+            Point(0.2): -0.214401233455065,
+            Point(0.4): -0.24273847930859488,
+            Point(0.6): -0.09488746599316666,
+            Point(0.8): -0.1690131707314828,
+            Point(1.0): -0.03727938104424598,
         })
 
         self.assertEqual(expected, result)
